5f187ef6a2fdac6b6fd534eafc1e52334895294d
Anis Abboud <anis.abboud@gmail.com>
2015-02-08 02:30:59 -0500

    database.yml

--- my_scripts/parse_testudo.py
+++ my_scripts/parse_testudo.py
@@ -1,12 +1,10 @@
 DEPARTMENTS = ['AASP', 'AAST', 'AGNR', 'AMSC', 'AMST', 'ANSC', 'ANTH', 'AOSC', 'ARAB', 'ARCH', 'AREC', 'ARHU', 'ARMY', 'ARSC', 'ARTH', 'ARTT', 'ASTR', 'BCHM', 'BIOE', 'BIOL', 'BIOM', 'BIPH', 'BMGT', 'BSCI', 'BSCV', 'BSGC', 'BSOS', 'BSST', 'BUAC', 'BUDT', 'BUFN', 'BULM', 'BUMK', 'BUMO', 'BUSI', 'CBMG', 'CCJS', 'CHBE', 'CHEM', 'CHIN', 'CHPH', 'CLAS', 'CLFS', 'CMLT', 'CMSC', 'COMM', 'CONS', 'CPET', 'CPGH', 'CPMS', 'CPPL', 'CPSA', 'CPSD', 'CPSF', 'CPSG', 'CPSN', 'CPSP', 'CPSS', 'DANC', 'ECON', 'EDCI', 'EDCP', 'EDHD', 'EDHI', 'EDMS', 'EDPS', 'EDSP', 'EDUC', 'ENAE', 'ENCE', 'ENCH', 'ENCO', 'ENEE', 'ENES', 'ENFP', 'ENGL', 'ENMA', 'ENME', 'ENNU', 'ENPM', 'ENPP', 'ENRE', 'ENSE', 'ENSP', 'ENST', 'ENTM', 'ENTS', 'EPIB', 'EXST', 'FILM', 'FIRE', 'FMSC', 'FOLA', 'FREN', 'GEMS', 'GEOG', 'GEOL', 'GERM', 'GREK', 'GVPT', 'HACS', 'HDCC', 'HEBR', 'HEIP', 'HESI', 'HESP', 'HHUM', 'HISP', 'HIST', 'HLSA', 'HLSC', 'HLTH', 'HONR', 'IMMR', 'INAG', 'INFM', 'INST', 'ISRL', 'ITAL', 'JAPN', 'JOUR', 'JWST', 'KNES', 'KORA', 'LARC', 'LASC', 'LATN', 'LBSC', 'LGBT', 'LING', 'MAIT', 'MATH', 'MEES', 'MIEH', 'MLAW', 'MOCB', 'MUED', 'MUSC', 'NACS', 'NFSC', 'NIAV', 'PERS', 'PHIL', 'PHYS', 'PLSC', 'PORT', 'PSYC', 'PUAF', 'RDEV', 'RELS', 'RUSS', 'SLAA', 'SLLC', 'SOCY', 'SPAN', 'SPHL', 'STAT', 'SURV', 'TDPS', 'THET', 'TLPL', 'UMEI', 'UNIV', 'URSP', 'USLT', 'VMSC', 'WMST']
 
-from urlparse import urlparse
-
 from lxml import html
-import requests
 import html2text
-import codecs
+import requests
 
+converter = html2text.HTML2Text()
 
 def Scrape(url, path, exclude_path=None):
   page = requests.get(url)
@@ -20,4 +18,14 @@ def Scrape(url, path, exclude_path=None):
 
   return text
 
-print Scrape('https://ntst.umd.edu/soc/201501/CMSC')
+def ID(x):
+  return '//div[@id="%s"]' % x
+def CLASS(x):
+  return '//*[contains(@class, "%s")]' % x
+def TD(w):
+  return '//td[@width="%d%%"]' % w
+def STYLE(name, value):
+  return '//*[contains(@style, "%s:%s") or contains(@style, "%s: %s") or contains(@style, "%s:%s;") or contains(@style, "%s: %s;")]' % (
+    name, value, name, value, name, value, name, value)
+
+print Scrape('https://ntst.umd.edu/soc/201501/CMSC', CLASS('course-id'))
