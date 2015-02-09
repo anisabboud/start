465cad88b265f87552d575ea69e016c1fff92c01
Kevin Young <kyoung18@terpmail.umd.edu>
2015-02-09 02:32:47 -0500

    improvements to parsing schema

--- my_scripts/parse_testudo.js
+++ my_scripts/parse_testudo.js
@@ -1,44 +1,87 @@
-var master = {
+var schema = {
   'course': {
+      'name': "",
       'sections': {
           'section': {
             'location': "",
-            'meetings': []
+            'meetings': {
+                'start_time': "",
+                'end_time' : "",
+                'days': []
+            }
           } 
       },
       'instructors': {
           'instructor': {
               'location': "",
               'email': "",
-              'meetings': []
+              'meetings': {
+                'start_time': "",
+                'end_time' : "",
+                'days': []
+            }
           } 
       },
   },
 
 };
 
+var master = {};
+
 var departments = ['AASP', 'AAST', 'AGNR', 'AMSC', 'AMST', 'ANSC', 'ANTH', 'AOSC', 'ARAB', 'ARCH', 'AREC', 'ARHU', 'ARMY', 'ARSC', 'ARTH', 'ARTT', 'ASTR', 'BCHM', 'BIOE', 'BIOL', 'BIOM', 'BIPH', 'BMGT', 'BSCI', 'BSCV', 'BSGC', 'BSOS', 'BSST', 'BUAC', 'BUDT', 'BUFN', 'BULM', 'BUMK', 'BUMO', 'BUSI', 'CBMG', 'CCJS', 'CHBE', 'CHEM', 'CHIN', 'CHPH', 'CLAS', 'CLFS', 'CMLT', 'CMSC', 'COMM', 'CONS', 'CPET', 'CPGH', 'CPMS', 'CPPL', 'CPSA', 'CPSD', 'CPSF', 'CPSG', 'CPSN', 'CPSP', 'CPSS', 'DANC', 'ECON', 'EDCI', 'EDCP', 'EDHD', 'EDHI', 'EDMS', 'EDPS', 'EDSP', 'EDUC', 'ENAE', 'ENCE', 'ENCH', 'ENCO', 'ENEE', 'ENES', 'ENFP', 'ENGL', 'ENMA', 'ENME', 'ENNU', 'ENPM', 'ENPP', 'ENRE', 'ENSE', 'ENSP', 'ENST', 'ENTM', 'ENTS', 'EPIB', 'EXST', 'FILM', 'FIRE', 'FMSC', 'FOLA', 'FREN', 'GEMS', 'GEOG', 'GEOL', 'GERM', 'GREK', 'GVPT', 'HACS', 'HDCC', 'HEBR', 'HEIP', 'HESI', 'HESP', 'HHUM', 'HISP', 'HIST', 'HLSA', 'HLSC', 'HLTH', 'HONR', 'IMMR', 'INAG', 'INFM', 'INST', 'ISRL', 'ITAL', 'JAPN', 'JOUR', 'JWST', 'KNES', 'KORA', 'LARC', 'LASC', 'LATN', 'LBSC', 'LGBT', 'LING', 'MAIT', 'MATH', 'MEES', 'MIEH', 'MLAW', 'MOCB', 'MUED', 'MUSC', 'NACS', 'NFSC', 'NIAV', 'PERS', 'PHIL', 'PHYS', 'PLSC', 'PORT', 'PSYC', 'PUAF', 'RDEV', 'RELS', 'RUSS', 'SLAA', 'SLLC', 'SOCY', 'SPAN', 'SPHL', 'STAT', 'SURV', 'TDPS', 'THET', 'TLPL', 'UMEI', 'UNIV', 'URSP', 'USLT', 'VMSC', 'WMST']
 
 for (var i = 0; i < departments.length; i++) {
   window.location = window.location.href + "201501/" + departments[i];
+
 
   $('#show-all-sections-button').click();
 
   setTimeout(function(){}, 6000);
 
   $('.course').each(function() {
-    console.log($(this).attr('id'));
+    var course_number = $(this).attr('id');
+    master[course_number] = {};
+
+    console.log($(this).attr('id')); // course number
+    
+    var name = $(this).find('.course-title').text();
+    master[course_number]['name'] = name;
+    
     console.log($(this).find('.course-title').text());
     $(this).find('.section').each(function() {
       var section = $(this);
+      var sectionId = section.find('.section-id').text().trim();
+      master[course_number]['sections'] = {};
+      master[course_number]['sections'][sectionId] = {};
+
       console.log('section-id: ' + section.find('.section-id').text().trim()); 
+      var instructor = section.find('.section-instructor').text();
+      
+      master[course_number]['instructors'] = {};
+      master[course_number]['instructors'][instructor] = {};
+
       console.log('section-instructor: ' + section.find('.section-instructor').text()); 
       console.log('class-days-container:');
+      
       section.find('.class-days-container .row').each(function() {
         var row = $(this);
+        var section_days_arr = row.find('.section-days').text().split(/(?=[A-Z])/); // array of
         console.log('section-days: ' + row.find('.section-days').text());
+        
+        var start_time = row.find('.class-start-time').text();
+        var end_time = row.find('.class-end-time').text();
+        
+        master[course_number]['sections'][sectionId]['meetings'] = {};
+        master[course_number]['sections'][sectionId]['meetings']['start_time'] = start_time;
+        master[course_number]['sections'][sectionId]['meetings']['end_time'] = end_time;
+        master[course_number]['sections'][sectionId]['meetings']['days'] = section_days_arr;
+
         console.log('class-start-time: ' + row.find('.class-start-time').text());
         console.log('class-end-time: ' + row.find('.class-end-time').text());
+        
+        var location = row.find('.building-code').text() + row.find('.class-room').text();
+        master[course_number]['sections'][sectionId]['location'] = location;
+        console.log('building-code: ' + row.find('.building-code').text());
         console.log('class-room: ' + row.find('.class-room').text());
       });
     }); 
@@ -46,24 +89,3 @@ for (var i = 0; i < departments.length; i++) {
   });
 
 }
-
-$('#show-all-sections-button').click();
-setTimeout(function(){}, 3000);
-$('.course').each(function() {
-  console.log($(this).attr('id'));
-  console.log($(this).find('.course-title').text());
-  $(this).find('.section').each(function() {
-    var section = $(this);
-    console.log('section-id: ' + section.find('.section-id').text().trim()); 
-    console.log('section-instructor: ' + section.find('.section-instructor').text()); 
-    console.log('class-days-container:');
-    section.find('.class-days-container .row').each(function() {
-      var row = $(this);
-      console.log('section-days: ' + row.find('.section-days').text());
-      console.log('class-start-time: ' + row.find('.class-start-time').text());
-      console.log('class-end-time: ' + row.find('.class-end-time').text());
-      console.log('class-room: ' + row.find('.class-room').text());
-    });
-  }); 
-  console.log(' ');
-});
